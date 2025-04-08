cabe4f0aa6c23551c74c35df2959e8e684573900
ela.md <ela.ahmadi06@gmail.com>
2025-04-08 19:25:34 -0400

    initial commit (folder structure)

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+
+import './Navbar.css'
+import { MdOutlineDarkMode, MdLightMode  } from "react-icons/md";
+
+
+
+
+function Navbar(){
+
+    return(
+       <h2>navbar</h2>
+    )
+}
+
+export default Navbar
