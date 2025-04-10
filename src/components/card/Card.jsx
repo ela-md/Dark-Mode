0be2c17efca8b625b9257868539e59c099abfcb9
ela.md <ela.ahmadi06@gmail.com>
2025-04-10 17:20:49 -0400

    making cards dynamic

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,16 @@
 import { useContext } from 'react';
 import './Card.css'
-import { FaReact } from "react-icons/fa";
 import { AppContext } from '../../context';
-function Card (){
+function Card ({icon, title}){
 
     const {theme} = useContext(AppContext)
 
     return(
         <div className={`card ${theme}`}>
             <p className="icon">
-            <FaReact />
+             {icon}
             </p>
-            <h2>card title</h2>
+            <h2>{title}</h2>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempore error ad tenetur aut velit et optio placeat soluta, earum pariatur officia, harum odit eaque veniam illum, voluptate obcaecati recusandae?
                 Laudantium nemo rerum eveniet possimus? Repellat voluptatibus ea nemo, vel blanditiis ullam, est dignissimos nostrum nobis nulla sequi mollitia eos temporibus perferendis ducimus earum quia excepturi id consequatur, voluptate obcaecati recusandae?  Laudantium nemo rerum eveniet possimus?    
--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -1,15 +1,44 @@
 import './Cards.css'
 import Card from './Card'
+import { FaReact } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaBootstrap } from "react-icons/fa";
 
 function Cards () {
+
+    const cards = [
+        {
+            id: 1,
+            title: 'react js',
+            icon: <FaReact />,
+        },
+
+        {
+            id: 2,
+            title: 'node js',
+            icon: <FaNodeJs />
+        },
+
+        {
+            id: 3,
+            title: 'tailwind css',
+            icon: <RiTailwindCssFill />
+        },
+
+        {
+            id: 4,
+            title: 'bootstrap',
+            icon: <FaBootstrap />
+        },
+    ]
 
     return(
     
        <div className="cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+     {cards.map((item) => (
+        <Card key={item.id} {...item} />
+     ))}
        </div>
 
     )
